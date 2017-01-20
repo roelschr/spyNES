702a1b6f0098c4e126f8e2b5d1fee95111abe452
ihoelscher <iigghh92>
2017-01-19 23:22:17 -0200

    ROM v1.

---
+++ main.py
@@ -0,0 +1,16 @@
+# import usefull packages
+import sys
+
+#import spyNES packages
+from rom import ROM
+
+
+def main():
+    assert len(sys.argv) > 1, "Not given any parameter."
+
+    fp = open(sys.argv[1],  "rb")
+    ROM(fp)
+
+
+if __name__ == "__main__":
+    main()
---
+++ rom.py
@@ -0,0 +1,35 @@
+KBsize = 1024
+
+class ROM:
+    sNES = "NES" # 3 bytes
+    idFormat = 0x1A # 1 byte
+    num16ROMBanks = 0x00 # 1 byte
+    num8ROMBanks = 0x00 # 1 byte
+    ctrl1 = 0x00 # 1 byte
+    ctrl2 = 0x00 # 1 byte
+    num8RAMBanks = 0x00 # 1 byte
+    reserved = [] # 7 byte
+
+    data = []
+
+    def __init__(self, romFile):
+        if  (romFile.read(3) == self.sNES):
+            _ = romFile.read(1)
+            self.num16ROMBanks = int(romFile.read(1).encode('hex'), 16)
+            self.num8ROMBanks = int(romFile.read(1).encode('hex'), 16)
+            self.ctrl1 = romFile.read(1).encode('hex')
+            self.ctrl2 = romFile.read(1).encode('hex')
+            self.num8RAMBanks = int(romFile.read(1).encode('hex'), 16)
+            self.reserved = romFile.read(7).encode('hex')
+
+            self.data = romFile.read(self.num16ROMBanks*16*KBsize + self.num8ROMBanks*8*KBsize)
+
+        else:
+            raise Exception("It is not a iNES game.")
+
+    def get_memory(self):
+        return self.data
+
+    def get(self, position, size):
+        assert size > 0, "Not a valid size."
+        return self.data[position:position+size]
